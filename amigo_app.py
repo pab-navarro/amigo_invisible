69a3ffbb601d8b8d0e097f6d705e8346f081a969
pab-navarro <99805022+pab-navarro@users.noreply.github.com>
2023-01-06 13:24:00 +0100

    Create amigo_app.py

---
+++ amigo_app.py
@@ -0,0 +1,11 @@
+import streamlit
+import pandas
+
+streamlit.title('My Parent New Healthy Diner')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
