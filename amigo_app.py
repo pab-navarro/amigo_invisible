ad004f65c8502c0858469a7d8a320505e5b5f5e5
pab-navarro <99805022+pab-navarro@users.noreply.github.com>
2023-01-06 13:26:41 +0100

    Update amigo_app.py

--- amigo_app.py
+++ amigo_app.py
@@ -1,5 +1,7 @@
 import streamlit
 import pandas
+import folium
+from streamlit_folium import st_folium
 
 streamlit.title('My Parent New Healthy Diner')
 streamlit.header('Breakfast Favorites')
@@ -9,3 +11,13 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+# center on Liberty Bell, add marker
+m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+folium.Marker(
+    [39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
+).add_to(m)
+
+# call to render Folium map in Streamlit
+st_data = st_folium(m, width=725)
