29c8e0bbb3a680b8092ba1e491875df7c742711b
pab-navarro <99805022+pab-navarro@users.noreply.github.com>
2023-01-07 10:12:09 +0100

    Update amigo_app.py

--- amigo_app.py
+++ amigo_app.py
@@ -40,7 +40,7 @@ streamlit.markdown("![Alt Text](https://media3.giphy.com/media/HW14ptNE7lJw4/200
 
 streamlit.markdown("""
 <table cellpadding="0" cellspacing="0" border="0" width="100%">
-<tr><td align="center"> <img src="https://media3.giphy.com/media/HW14ptNE7lJw4/200.gif?cid=790b7611xq4pppva4mw60avv6h24ja5e2lx1v9t8oos6tdmd&rid=200.gif&ct=g> </td></tr>
+<tr><td align="center"> <img src="https://media3.giphy.com/media/HW14ptNE7lJw4/200.gif?cid=790b7611xq4pppva4mw60avv6h24ja5e2lx1v9t8oos6tdmd&rid=200.gif&ct=g"> </td></tr>
 </tr> </table>""", unsafe_allow_html=True)
 
 
